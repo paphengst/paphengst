6ee04dedf5a774ef1d251555e7b2e1b47d0fa959
paphengst <paphengst@users.noreply.github.com>
2016-10-31 01:40:15 +0100

    Create ichbrauchdenhengst.py

---
+++ ichbrauchdenhengst.py
@@ -0,0 +1,4 @@
+# Ordner "results" für zukünftige Ergebnisse erstellen
+# Ordner "upload" erstellen plus die Infodatei "!!! HIER SOLLEN DIE DATEIEN HOCHGELADEN WERDEN !!!"
+# Menüskript paphengst.py schreiben (Versuchsauswertung, Link zur HTML anzeigen, Update)
+# Updateablauf: paphengst-Ordner löschen (alte results und uploads bleiben erhalten), per git neuen Code laden, paphengst/ichbrauchdenhengst.py ausführen für neue Installation

627ce16e444c28499e756752493d58bf14ee0a07
paphengst <paphengst@users.noreply.github.com>
2016-11-01 15:38:42 +0100

    Create machdichfrisch.py

---
+++ machdichfrisch.py
@@ -0,0 +1,17 @@
+import os
+
+# Hauptverzeichnis ermitteln
+maindir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../")
+
+# Alle Dateien und Ordner im Ordner "paphengst" und seinen Unterordnern löschen
+for path, folders, files in os.walk(os.path.join(maindir, "paphengst/"), topdown=False):
+    for file in files:
+        os.remove(os.path.join(path, file))
+    for folder in folders:
+        os.rmdir(os.path.join(path, folder))
+
+# Dateien frisch von Github laden
+os.system("git clone --depth=1 https://github.com/paphengst/paphengst")
+
+# Dateistruktur (neu) initialisieren
+os.system("python3 paphengst/ichbrauchedich.py")

e6df01d353304fe5dcf9dae409124ba714dcb285
paphengst <paphengst@users.noreply.github.com>
2016-11-04 22:57:21 +0100

    Update paphengstlib.py

--- paphengstlib.py
+++ paphengstlib.py
@@ -10,5 +10,6 @@ maindir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../")
 # Weitere Verzeichnisse speichern
 resultsdir = os.path.join(maindir, "results/")
 uploaddir = os.path.join(maindir, "uploads/")
-pythondir = os.path.join(maindir, "paphengst/python/")
-htmldir = os.path.join(maindir, "paphengst/html/")
+libdir = os.path.join(maindir, "paphengst/")
+pythondir = os.path.join(libdir, "python/")
+htmldir = os.path.join(libdir, "html/")

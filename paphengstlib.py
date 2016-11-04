c53d4a7afe605456dc5777ea68049fedab3e5c9c
paphengst <paphengst@users.noreply.github.com>
2016-11-04 22:51:54 +0100

    Update paphengstlib.py

--- paphengstlib.py
+++ paphengstlib.py
@@ -6,3 +6,9 @@ import re
 
 # Hauptverzeichnis ermitteln
 maindir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../")
+
+# Weitere Verzeichnisse speichern
+resultsdir = os.path.join(maindir, "results/")
+uploaddir = os.path.join(maindir, "uploads/")
+pythondir = os.path.join(maindir, "paphengst/python/")
+htmldir = os.path.join(maindir, "paphengst/html/")

44d0781754f8ca49ae1afca9570000f53626169d
paphengst <paphengst@users.noreply.github.com>
2016-11-06 16:30:12 +0100

    Update 243.py

--- python/243.py
+++ python/243.py
@@ -1,50 +1,32 @@
 # paphengstlib importieren
 from paphengstlib import *
 
+# Versuchsauswertung
+latexschreiben(r"blabla blabla blabla ")
+latexschreiben(r"\\")
+latexschreiben(r"$ \sqrt{77} $")
+latexkompilieren()
+
+sys.exit()
+
+def neuegrafikdatei(matplotlibgraph):
+    pngfile = os.path.join(resultsdir, "243-1.png")
+    matplotlibgraph.savefig(pngfile)
+    return pngfile
+
 #Beispielcode für matplotlib-Plots
 """
 import numpy
 import sympy
 print(numpy.pi)
 print(sympy.diff("x**2", "x"))
+"""
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(range(100))
-fig.savefig("graph.png")
-"""
+a = neuegrafikdatei(fig)
 
-#LaTeX output nach 243.tex
-
-versuchsnummer = "243"
-resultsdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../results/")
-texfile = os.path.join(resultsdir, versuchsnummer + ".tex")
-pdffile = os.path.join(resultsdir, versuchsnummer + ".pdf")
-pngfile = os.path.join(resultsdir, versuchsnummer + ".png")
-
-s = r"""\documentclass[12pt]{article}
-\usepackage[utf8]{inputenc}
-\usepackage{amsmath}
-\setlength{\parindent}{0pt}
-\begin{document}
-
-blabla
-
-\end{document}
-"""
-text_file = open(texfile, "w")
-text_file.write(s)
-text_file.close()
-
-#PDF erzeugen
-os.system("pdflatex --output-directory=" + resultsdir + " " + texfile)
-
-#PNGs erzeugen
-os.system("convert -density 150 " + pdffile + " " + pngfile)
-#ergibt z-0.png, z-1.png, ..., z-33.png
-#anzahl der PNGs rausfinden und HTML entsprechend erzeugen:
-versuchsnummer = "z"
-items = ['a.txt', 'z.png', 'z-0.png', 'z-33.PNG', 'zdad-0.png', 'z-0-0.png', 'zdd.png', 'za.png']
-matches = re.findall(versuchsnummer + r"(?:-[0-9]*?)?\.png", "".join(items), re.IGNORECASE)
+s += r"\\ \\ \includegraphics[width=10cm]{" + a + r"}"
